8cb3f4a6d51b4bb818b9986da9f3d3b20d4c5dee
DHIRAJ GURUNG <gdhiraj030@gmail.com>
2026-04-18 09:13:31 +0545

    Time: 42 ms (95.25%), Space: 53.2 MB (96.01%) - LeetHub

--- 2723-add-two-promises/2723-add-two-promises.js
+++ 2723-add-two-promises/2723-add-two-promises.js
@@ -4,9 +4,9 @@
  * @return {Promise}
  */
 var addTwoPromises = async function(promise1, promise2) {
-    const [value1,value2]=await Promise.all([promise1,promise2])
-
-    return value1 + value2;
+    let value1=await promise1
+    let value2=await promise2
+    return value1 + value2
 };
 
 /**
