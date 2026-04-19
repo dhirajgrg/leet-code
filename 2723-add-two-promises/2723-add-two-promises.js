c94043bd5cf5cbe6a1d4524145d5f2e0fd5ee202
DHIRAJ GURUNG <gdhiraj030@gmail.com>
2026-04-19 20:29:51 +0545

    Time: 59 ms (26.94%), Space: 54.7 MB (22.26%) - LeetHub

--- 2723-add-two-promises/2723-add-two-promises.js
+++ 2723-add-two-promises/2723-add-two-promises.js
@@ -3,9 +3,8 @@
  * @param {Promise} promise2
  * @return {Promise}
  */
-var addTwoPromises = async function(promise1, promise2) {
-    let value1=await promise1
-    let value2=await promise2
+var addTwoPromises = async function (promise1, promise2) {
+    let [value1, value2] = await Promise.all([promise1, promise2])
     return value1 + value2
 };
 
